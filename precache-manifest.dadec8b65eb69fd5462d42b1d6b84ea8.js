28a98a898f77e54581db38e1b0c70b4c33b79496
dsouzajoy <joydsouza3380@gmail.com>
2020-06-02 19:26:49 +0530

    Updates

---
+++ precache-manifest.dadec8b65eb69fd5462d42b1d6b84ea8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c98f7ddbe6842e00972cdac310d1aa3",
+    "url": "/corona-tracker/index.html"
+  },
+  {
+    "revision": "d39eb2b6c2cb97e195a3",
+    "url": "/corona-tracker/static/css/main.40b7e2be.chunk.css"
+  },
+  {
+    "revision": "48ad4625e2c43bb1af91",
+    "url": "/corona-tracker/static/js/2.185b329a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/corona-tracker/static/js/2.185b329a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d39eb2b6c2cb97e195a3",
+    "url": "/corona-tracker/static/js/main.3c581388.chunk.js"
+  },
+  {
+    "revision": "7e7c08e4a6218267afe9",
+    "url": "/corona-tracker/static/js/runtime-main.1b83197d.js"
+  }
+]);
