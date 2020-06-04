fa1cf87dbcf75d6a7790bd19ebe37c9e629b9afc
Praneeth RK <praneethrk17@gmail.com>
2020-06-04 12:18:03 +0530

    minor changes

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
     return (
         <div className="footer">
             <div className="authors">
-                Made with <i className="fas fa-heart"></i> by Joy Dsouza, Praneeth R K and Niranjan Malya.
+                Made with <i className="fas fa-heart"></i> by Joy Dsouza, Praneeth RK and Niranjan Malya.
             </div>
             <div className="copyrights">
                 &copy; {new Date().getFullYear()} All Rights Reserved.
