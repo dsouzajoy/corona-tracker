60f2eef5b59c5470a49137f6ee1ffc6e078f1cd6
dsouzajoy <joydsouza3380@gmail.com>
2020-06-15 14:00:05 +0530

    Updates

---
+++ precache-manifest.2adc576b2f73f5f56d870f669a3e4e11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a52b5c0ee198d602ffdd5530ba4664f",
+    "url": "/corona-tracker/index.html"
+  },
+  {
+    "revision": "b88e91b444cabf59cd27",
+    "url": "/corona-tracker/static/css/main.3f2f5a6b.chunk.css"
+  },
+  {
+    "revision": "f8cf3f0c29a9551a49f8",
+    "url": "/corona-tracker/static/js/2.c1cd23eb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/corona-tracker/static/js/2.c1cd23eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b88e91b444cabf59cd27",
+    "url": "/corona-tracker/static/js/main.1aa887ca.chunk.js"
+  },
+  {
+    "revision": "7e7c08e4a6218267afe9",
+    "url": "/corona-tracker/static/js/runtime-main.1b83197d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/corona-tracker/precache-manifest.7e31fee8f8d96c83a33ec4f2ed6c6935.js"
+  "/corona-tracker/precache-manifest.2adc576b2f73f5f56d870f669a3e4e11.js"
 );
 
 self.addEventListener('message', (event) => {
